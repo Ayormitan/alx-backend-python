99d1f461bb02998fbdd0c11dcfa2d80790e39b76
Ayormitan <kolawoleabaz97@gmail.com>
2024-10-14 10:29:06 -0700

    Return average time float

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
-
+import time
 from concurrent_coroutines import wait_n
 async def measure_time(n, max_delay) -> float:
     start_time = time.time()
-    wait_n(n, max_dealy)
+    await wait_n(n, max_delay)
     end_time = time.time()
     total_time = end_time - start_time
     return total_time / n
