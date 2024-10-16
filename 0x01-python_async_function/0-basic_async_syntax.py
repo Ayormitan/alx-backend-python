5959b19906189759d7974732feea0f6b3edbdeae
Ayormitan <kolawoleabaz97@gmail.com>
2024-10-16 03:11:30 -0700

    Asychronous defination

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+""" Define asychroneous routine"""
 import asyncio
 import random
 
-async def wait_random(max_delay=10):
-    # Generate a random delay between 0 and max_delay(10)
-    delay_value = random.uniform (0, max_delay)
-    #Await for the random delay
+async def wait_random(max_delay: int = 10) -> float:
+    """Generate a random delay between 0 and max_delay(10)"""
+    delay_value = random.uniform(0, max_delay)
+    """Await for the random delay"""
     await asyncio.sleep(delay_value)
-    # Return delay value
+    """Return delay value"""
     return delay_value
